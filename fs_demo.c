99bfc212d406a44480b3d99e5ceddd1a49170508
Rumen <rumenmitov@protonmail.com>
2026-06-19 10:11:52 +0200

    read example

--- fs_demo.c
+++ fs_demo.c
@@ -1,24 +1,28 @@
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdint.h>
 
 uint32_t phantom_mkdir(const char *path);
 uint32_t phantom_fwrite(const char *path, void *buf, uint32_t bufsize);
-uint32_t phantom_rename(const char *oldpath, const char *newpath);           
+uint32_t phantom_rename(const char *oldpath, const char *newpath);
 uint32_t phantom_fread(const char *path, void *buf, uint32_t bufsize);
+uint64_t phantom_gettime_us(void);
 
-static char *message = "baz";
+enum
+{
+  READS = 10,
+};
+
 
 int main(int argc, char *argv[])
 {
-	char *buf = malloc(strlen(message) + 1);
-	buf[strlen(message)] = 0;
+  char message[1024];
 
-	(void)phantom_mkdir("foo");
-	(void)phantom_fwrite("foo/bar.txt", message, strlen(message));
-	(void)phantom_rename("foo/bar.txt", "foo/bat.txt");
-	(void)phantom_fread("foo/bat.txt", buf, strlen(message));
+  uint64_t start = phantom_gettime_us();
 
-	return (strncmp(message, buf, strlen(message)));
+  for (int i = 0; i < READS; ++i) {
+    (void)phantom_fread("bar", message, sizeof(message));
+  }
+
+  uint64_t end = phantom_gettime_us();
+
+	return end - start;
 }
